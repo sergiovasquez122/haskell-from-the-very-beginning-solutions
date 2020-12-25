8bf84facbf257cf3f6485fb6e8d05aba6e05d817
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-12-25 13:17:30 -0800

    added to exercise 5

--- chapter_5/exercise6.hs
+++ chapter_5/exercise6.hs
@@ -4,3 +4,9 @@ sort (x:xs) = let
 	insert x (y:ys) = if x <= y then x:y:ys else y:(insert x ys)
 	in 
 		insert x (sort xs)	
+
+sortComplete [] = []
+sortComplete (x:xs) = insert x (sort xs)	
+	where 
+	insert x [] = [x]
+	insert x (y:ys) = if x <= y then x:y:ys else y:(insert x ys)
