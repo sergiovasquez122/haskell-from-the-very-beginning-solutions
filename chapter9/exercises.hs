2c83e0fa7c26c41105a93a1b8d943341b6ec7dfb
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-12-30 14:33:35 -0800

    added exercise solution

--- chapter9/exercises.hs
+++ chapter9/exercises.hs
@@ -15,6 +15,24 @@ mapll = map . map . map
 -- 4. Write a function truncateList which takes an integer and a list of lists, and returns a list of lists, each of which has been truncated to the given length. If a list is shorter than the given length, it is unchanged. Make use of partial applications.
 -- truncateList :: (Ord a, Num a) => a -> [b] -> [b]
 -- truncateLists :: (Ord a, Num a) => a -> [[b]] -> [[b]]
+take' :: (Ord a, Num a) => a -> [b] -> [b]
+length' :: (Num a) => [b] -> a
+map' :: (a->b) -> [a] -> [b]
 
--- truncateList n l = if length l >= n then (take n l) else l
--- truncateLists l = (map truncateList) l 
+map' f [] = []
+map' f (x:xs) = f x : map' f xs
+
+length' [] = 0
+length' (x:xs) = 1 + length' xs
+
+take' 0 _ = []
+take' n (x:xs) = x : take' (n - 1) xs
+
+truncateList n l = if length' l >= n then (take' n l) else l
+truncateLists l = (map' truncateList) l 
+-- 5. Write a function which takes a list of lists of numbers and return the list composed of all the first elements of the lists. If a list is empty, a given number should be used in place of its first element.
+firstElt :: a -> [a] -> a
+firstElts :: a -> [[a]] -> [a]
+firstElt n [] = n
+firstElt n (x:_) = x
+firstElts n = map' (firstElt n)
