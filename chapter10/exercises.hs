51e5240d610f73306c9ec6047e6e5138ce7c4506
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-12-30 15:25:59 -0800

    added solution

--- chapter10/exercises.hs
+++ chapter10/exercises.hs
@@ -1,2 +1,7 @@
 -- 1. Design a new type Rect for representing rectangles. Treat squares as a special case
-data Rect a = Square a | Rect a a deriving Show
+data Rect a = Square a | Rectangle a a deriving Show
+
+calculateArea :: (Num a) => Rect a -> a
+
+calculateArea (Square a) = a * a
+calculateArea (Rectangle l w) = l * w
