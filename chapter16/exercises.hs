e616809b3652c9e3369343ce180b7eac6728510d
Sergio Vasquez <sergiovasquez122@gmail.com>
2021-01-01 17:04:26 -0800

    added solution

--- chapter16/exercises.hs
+++ chapter16/exercises.hs
@@ -4,6 +4,11 @@ lowerString :: String -> String
 lowerString x = map toLower x
 -- 2. Use the functions dropWhile and takeWhile to isolate the section of a list which contain positive numbers. For example, isolate [-6, -4, -1, 0, 1, 2, 4, 5, 3, 2, -1, -9] should yield the list [1, 2, 4, 5, 3, 2]
 isolate xs = takeWhile (\x-> x > 0) (dropWhile (\x->x<=0) xs)
+-- 3. The word otherwise for guard equations is not really part of the Haskell Language, but is defined in the Standard Prelude. Find or deduce its definition.
+-- :type otherwise
+-- otherwise :: Bool
+-- otherwise == True
+-- True
 -- 5. The functions ord and chr from the Data.Char module convert between numbers and characters. Use these functions to write a function toLower which converts an upper case letter to lower case, leaving any other characters alone. This is a home-made version of the functions you used to solve Question 1 above.
 toLower' :: Char -> Char
 
