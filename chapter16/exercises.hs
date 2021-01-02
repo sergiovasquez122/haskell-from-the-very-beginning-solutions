9968db60f314a91a999a217db4c4e38f5867deb8
Sergio Vasquez <sergiovasquez122@gmail.com>
2021-01-01 16:54:46 -0800

    added solution

---
+++ chapter16/exercises.hs
@@ -0,0 +1,16 @@
+import Data.Char
+-- 1. Use a function from the Data.Char module to write a function which replaces all upper case characters in a given string with lower case ones.
+lowerString :: String -> String
+lowerString x = map toLower x
+-- 2. Use the functions dropWhile and takeWhile to isolate the section of a list which contain positive numbers. For example, isolate [-6, -4, -1, 0, 1, 2, 4, 5, 3, 2, -1, -9] should yield the list [1, 2, 4, 5, 3, 2]
+-- 5. The functions ord and chr from the Data.Char module convert between numbers and characters. Use these functions to write a function toLower which converts an upper case letter to lower case, leaving any other characters alone. This is a home-made version of the functions you used to solve Question 1 above.
+toLower' :: Char -> Char
+
+toLower' x = let 
+             ord_value = ord x
+             ord_value_of_A = ord 'A'
+             ord_value_of_a = ord 'a'
+             in
+	     if x >= 'A' && x <= 'Z'
+	     then chr (ord_value - ord_value_of_A + ord_value_of_a)
+             else x
