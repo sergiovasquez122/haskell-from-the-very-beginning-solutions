d4b3c08d840db195089def3d9f0b41becff9fc37
Sergio Vasquez <sergiovasquez122@gmail.com>
2021-01-03 11:56:19 -0800

    added solution

--- chapter14/exercises.hs
+++ chapter14/exercises.hs
@@ -1,6 +1,6 @@
 import Text.Read
 import System.IO
-
+-- Q1. Write a function to build an IO action which, when run, prints a list of numbers to the screen in the saem format Haskell uses
 printListInner :: Show a => [a] -> IO()
 printList :: Show a => [a] -> IO()
 
@@ -18,3 +18,23 @@ printList xs =
 	do putStr "["
            printListInner xs
            putStrLn "]"
+-- Q2. Write an IO action to read three integers from the user, and return them as a tuple. 
+getIntegerMaybe :: IO (Maybe Integer) 
+getIntegerMaybe = 
+	do 
+	line <- getLine
+	return (readMaybe line :: Maybe Integer)
+
+getIntegerRobust = 
+	do i <- getIntegerMaybe
+           case i of 
+	      Nothing -> do putStrLn "Not a number, try again"
+		     	    getIntegerRobust
+	      (Just x) -> return x
+
+getTriple = 
+        do	
+	x <- getIntegerRobust
+        y <- getIntegerRobust
+        z <- getIntegerRobust
+        return (x, y, z)
